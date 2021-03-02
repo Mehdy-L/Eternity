8acba9be86dce0d7c61f72a95402563c7fe92c8d
Mehdy-L <mehdy.lacroix@epita.fr>
2021-03-02 19:23:59 +0100

    test de la premiere version

---
+++ Eternity/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        //Récuperer le script PlayerMotor
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        // Calculer la vélocité (vitesse) du mouvement de notre joueur
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMove;
+        Vector3 moveVertical = transform.forward * zMove;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+    }
+}

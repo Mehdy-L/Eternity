fd6e90f6852bcc664c167d694f792264e3e200fd
Desir Michel <80551991+michel0716@users.noreply.github.com>
2021-04-14 15:19:07 +0200

    a mehdy
    push

---
+++ Eternity/Assets/Scripts/Pausemenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausemenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMenu ()
+    {
+        Debug.Log(2);
+    }
+    public void QuitGame()
+    {
+        Debug.Log(2);
+    }
+}
